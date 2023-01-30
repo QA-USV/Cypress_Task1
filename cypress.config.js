8b0456f1d6a4f9509a94cd66aeff3ee38a62297c
Sergei US <ustimenko.sv@gmail.com>
2023-01-30 16:39:11 +0300

    Final commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
- 
+  projectId: "gamjr9",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
